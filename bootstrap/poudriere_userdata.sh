435c559694261305f7275ebe697243bff75276b2
Robert James Hernandez <rob@sarcasticadmin.com>
2019-05-08 22:03:34 -0700

    Adding trap to scaledown asg on any exit

--- bootstrap/poudriere_userdata.sh
+++ bootstrap/poudriere_userdata.sh
@@ -9,16 +9,38 @@ export PATH=$PATH:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/
 # like AMI NAME, SIGNING_KEY,
 # S3_BUCKET, SIGNING_S3_KEY
 . /etc/terraform.facts
-ARCH=$(echo ${AMI_NAME} | cut -d '-' -f3)
-VERSION=$(echo ${AMI_NAME} | cut -d ' ' -f2 | cut -d '-' -f1)
-ABI="FreeBSD:$(echo ${VERSION} | cut -d '.' -f1):${ARCH}"
+CURL_FLAGS="-L --connect-timeout 10 --max-time 120"
+ARCH=$(echo "${AMI_NAME}" | cut -d '-' -f3)
+VERSION=$(echo "${AMI_NAME}" | cut -d ' ' -f2 | cut -d '-' -f1)
+ABI="FreeBSD:$(echo "${VERSION}" | cut -d '.' -f1):${ARCH}"
 
-env ASSUME_ALWAYS_YES=YES pkg bootstrap
+install_pkgs(){
+  env ASSUME_ALWAYS_YES=YES pkg bootstrap
 
-# awscli already presenet in ec2 instance
-# but installed after this bootstrap runs
-# so we must install it now
-pkg install -y poudriere awscli curl
+  # awscli already presenet in ec2 instance
+  # but installed after this bootstrap runs
+  # so we must install it now
+  pkg install -y poudriere awscli curl
+}
+
+scale_down(){
+  ## Turn autoscaling off
+  INSTANCE_ID=$(curl $CURL_FLAGS http://169.254.169.254/latest/meta-data/instance-id)
+  REGION=$(curl $CURL_FLAGS http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
+  echo "[INFO] Scaling $INSTANCE_ID to 0"
+  AUTOSCALING_GROUP=$(aws ec2 describe-instances \
+              --instance-id ${INSTANCE_ID} \
+              --region ${REGION} \
+              --query 'Reservations[].Instances[].[Tags[?Key==`aws:autoscaling:groupName`].Value]' \
+              --output text)
+  aws autoscaling set-desired-capacity --auto-scaling-group-name ${AUTOSCALING_GROUP} --desired-capacity 0 --region ${REGION}
+  # Consider getting log off the instance
+  # aws s3 cp "/var/log/bootstrap.log" "s3://${S3_BUCKET}/${DATE}/bootstrap-$(date +%s).log"
+}
+
+install_pkgs
+
+trap scale_down EXIT
 
 # Key stuff
 mkdir -p /usr/local/etc/ssl/keys
@@ -40,14 +62,3 @@ poudriere bulk -f /usr/local/etc/poudriere-list -j ${ABI}
 # Url will be hostname/pkgs/ABI
 aws s3 sync "/data/packages/${ABI}-default/.latest/" "s3://${S3_BUCKET}/repos/${ABI}" --delete
 aws s3 sync "/data/logs/bulk/${ABI}-default/latest/" "s3://${S3_BUCKET}/results/${ABI}" --delete
-
-# Turn autoscaling off
-CURL_FLAGS="-L --connect-timeout 10 --max-time 120"
-INSTANCE_ID=$(curl $CURL_FLAGS http://169.254.169.254/latest/meta-data/instance-id)
-REGION=$(curl $CURL_FLAGS http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
-AUTOSCALING_GROUP=$(aws ec2 describe-instances \
-            --instance-id ${INSTANCE_ID} \
-            --region ${REGION} \
-            --query 'Reservations[].Instances[].[Tags[?Key==`aws:autoscaling:groupName`].Value]' \
-            --output text)
-aws autoscaling set-desired-capacity --auto-scaling-group-name ${AUTOSCALING_GROUP} --desired-capacity 0 --region ${REGION}
